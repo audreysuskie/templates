e1b1931db577271c162bd53360a546bc86d1f4f2
Audrey Fernandez <audrey@audreydesigns.com>
2023-03-21 16:52:07 -0700

    added custom calendar

--- public/js/archive/calendar.js
+++ public/js/archive/calendar.js
@@ -1,6 +1,8 @@
 let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
+let selectYear = document.getElementById('year');
+let selectMonth = document.getElementById('month');
 
 let months = [
   'January',
@@ -32,15 +34,20 @@ function previous() {
   showCalendar(currentMonth, currentYear);
 }
 
+// function jump() {
+//   currentYear = parseInt(selectYear.value);
+//   currentMonth = parseInt(selectMonth.value);
+//   showCalendar(currentMonth, currentYear);
+// }
+
 function showCalendar(month, year) {
   let firstDay = new Date(year, month).getDay();
   let daysInMonth = 32 - new Date(year, month, 32).getDate();
 
-  let tbl = document.querySelector('.calendar-days'); // body of the calendar
+  let tbl = document.getElementById('calendar-body'); // body of the calendar
 
-  // clearing all previous calendar day cells
-  tbl.innerHTML =
-    '<div class="calendar-weekday">Sun</div><div class="calendar-weekday">Mon</div><div class="calendar-weekday">Tue</div><div class="calendar-weekday">Wed</div><div class="calendar-weekday">Thu</div><div class="calendar-weekday">Fri</div><div class="calendar-weekday">Sat</div>';
+  // clearing all previous cells
+  tbl.innerHTML = '';
 
   // filing data about month and in the page via DOM.
   monthAndYear.innerHTML = months[month] + ' ' + year;
@@ -51,30 +58,36 @@ function showCalendar(month, year) {
   let date = 1;
   for (let i = 0; i < 6; i++) {
     // creates a table row
-    let row = document.querySelector('.calendar-days');
+    let row = document.createElement('tr');
 
     //creating individual cells, filing them up with data.
-    for (let j = 0; j < 35; j++) {
+    for (let j = 0; j < 7; j++) {
       if (i === 0 && j < firstDay) {
-        let cell = document.createElement('div');
+        let cell = document.createElement('td');
         let cellText = document.createTextNode('');
         cell.appendChild(cellText);
         row.appendChild(cell);
       } else if (date > daysInMonth) {
         break;
       } else {
-        let cell = document.createElement('div');
-        cell.classList.add('calendar-day');
+        let cell = document.createElement('td');
+        cell.classList.add('booking');
         cell.setAttribute('value', date);
+        cell.classList.add('past');
         let cellText = document.createTextNode(date);
-        if (date > today.getDate() || month > today.getMonth()) {
-          cell.classList.add('available');
+        if (
+          date > today.getDate() &&
+          year >= today.getFullYear() &&
+          month >= today.getMonth()
+        ) {
+          cell.classList.remove('past');
         } else if (
           date === today.getDate() &&
           year === today.getFullYear() &&
           month === today.getMonth()
         ) {
           cell.classList.add('today-bg');
+          cell.classList.remove('past');
         }
         // color today's date
         cell.appendChild(cellText);
@@ -82,14 +95,16 @@ function showCalendar(month, year) {
         date++;
       }
     }
+
+    tbl.appendChild(row); // appending each row into calendar body.
   }
 }
 
 const booking = document.getElementById('book-date');
 
 $(document).ready(function () {
-  $('.calendar-day').click(function () {
-    $('.calendar-day').removeClass('selected');
+  $('.booking').click(function () {
+    $('.booking').removeClass('selected');
     booking.value = calmonth + ' ' + $(this).attr('value') + ', ' + calyear;
     $(this).addClass('selected');
   });
